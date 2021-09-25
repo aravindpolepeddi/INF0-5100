c910fa7876477f64af225e1a64a4fe955b5d7a5d
Polepeddi aravind <polepeddi.a@northeastern.edu>
2021-09-24 23:51:15 -0400

    Assignment_1

---
+++ Lab_1/src/model/Profile.java
@@ -0,0 +1,211 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.util.Date;
+
+/**
+ *
+ * @author aravindpolepeddi
+ */
+public class Profile {
+    
+    private String Name;
+    private String Geographic_data;
+    private Date Date_of_birth;
+    private long Telephone_numbers;
+    private long FAX_number;
+    private String Email_addresses;
+    private long Social_Security_number;
+    private String Medical_record_number;
+    private String Health_plan_beneficiary_number;
+    private long Bank_account_numbers;
+    private long License_number;
+    private String Vehicle_identifiers;
+    private String Vehicle_serial_numbers;
+    private String license_plates;
+    private long Device_identifiers;
+    private String LinkedIn;
+    private String IP_addresses;
+    private String Biometric_identifiers;
+    private String Full_face_photos;
+    private String comparable_images;
+    private String unique_identifying_num;
+
+    public String getUnique_identifying_num() {
+        return unique_identifying_num;
+    }
+
+    public void setUnique_identifying_num(String unique_identifying_num) {
+        this.unique_identifying_num = unique_identifying_num;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+
+    public String getGeographic_data() {
+        return Geographic_data;
+    }
+
+    public void setGeographic_data(String Geographic_data) {
+        this.Geographic_data = Geographic_data;
+    }
+
+    public Date getDate_of_birth() {
+        return Date_of_birth;
+    }
+
+    public void setDate_of_birth(Date Date_of_birth) {
+        this.Date_of_birth = Date_of_birth;
+    }
+
+    public long getTelephone_numbers() {
+        return Telephone_numbers;
+    }
+
+    public void setTelephone_numbers(long Telephone_numbers) {
+        
+        this.Telephone_numbers = Telephone_numbers;
+    }
+
+    public long getFAX_number() {
+        return FAX_number;
+    }
+
+    public void setFAX_number(long FAX_number) {
+        this.FAX_number = FAX_number;
+    }
+
+    public String getEmail_addresses() {
+        return Email_addresses;
+    }
+
+    public void setEmail_addresses(String Email_addresses) {
+        this.Email_addresses = Email_addresses;
+    }
+
+    public long getSocial_Security_number() {
+        return Social_Security_number;
+    }
+
+    public void setSocial_Security_number(long Social_Security_number) {
+        this.Social_Security_number = Social_Security_number;
+    }
+
+    public String getMedical_record_number() {
+        return Medical_record_number;
+    }
+
+    public void setMedical_record_number(String Medical_record_number) {
+        this.Medical_record_number = Medical_record_number;
+    }
+
+    public String getHealth_plan_beneficiary_number() {
+        return Health_plan_beneficiary_number;
+    }
+
+    public void setHealth_plan_beneficiary_number(String Health_plan_beneficiary_number) {
+        this.Health_plan_beneficiary_number = Health_plan_beneficiary_number;
+    }
+
+    public long getBank_account_numbers() {
+        return Bank_account_numbers;
+    }
+
+    public void setBank_account_numbers(long Bank_account_numbers) {
+        this.Bank_account_numbers = Bank_account_numbers;
+    }
+
+    public long getLicense_number() {
+        return License_number;
+    }
+
+    public void setLicense_number(long License_number) {
+        this.License_number = License_number;
+    }
+
+    public String getVehicle_identifiers() {
+        return Vehicle_identifiers;
+    }
+
+    public void setVehicle_identifiers(String Vehicle_identifiers) {
+        this.Vehicle_identifiers = Vehicle_identifiers;
+    }
+
+    public String getVehicle_serial_numbers() {
+        return Vehicle_serial_numbers;
+    }
+
+    public void setVehicle_serial_numbers(String Vehicle_serial_numbers) {
+        this.Vehicle_serial_numbers = Vehicle_serial_numbers;
+    }
+
+    public String getLicense_plates() {
+        return license_plates;
+    }
+
+    public void setLicense_plates(String license_plates) {
+        this.license_plates = license_plates;
+    }
+
+    public long getDevice_identifiers() {
+        return Device_identifiers;
+    }
+
+    public void setDevice_identifiers(long Device_identifiers) {
+        this.Device_identifiers = Device_identifiers;
+    }
+    
+    public String getLinkedIn() {
+        return LinkedIn;
+    }
+
+    public void setLinkedIn(String LinkedIn) {
+        this.LinkedIn = LinkedIn;
+    }
+
+    public String getIP_addresses() {
+        return IP_addresses;
+    }
+
+    public void setIP_addresses(String IP_addresses) {
+        this.IP_addresses = IP_addresses;
+    }
+
+    public String getBiometric_identifiers() {
+        return Biometric_identifiers;
+    }
+
+    public void setBiometric_identifiers(String Biometric_identifiers) {
+        this.Biometric_identifiers = Biometric_identifiers;
+    }
+
+    public String getFull_face_photos() {
+        return Full_face_photos;
+    }
+
+    public void setFull_face_photos(String Full_face_photos) {
+        this.Full_face_photos = Full_face_photos;
+    }
+
+    public String getComparable_images() {
+        return comparable_images;
+    }
+
+    public void setComparable_images(String comparable_images) {
+        this.comparable_images = comparable_images;
+    }
+
+    
+
+    
+    
+}
